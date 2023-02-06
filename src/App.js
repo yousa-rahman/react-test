b2875a13fbe59ae632970f5cb956a85b6f5c631a
yousufodoc <111130899+yousufodoc@users.noreply.github.com>
2023-02-06 19:35:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {Button, Form, Card, Spinner} from "react-bootstrap";
+
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { URL } from "./constants";
 
 function App() {
+  const [sectors, setSectors] = useState([]);
+  const [name, setName] = useState("");
+  const [selectedSectors, setSelectedSectors] = useState([]);
+  const [isAgreed, setIsAgreed] = useState(false);
+
+  useEffect(() => {
+    getSectorData();
+  }, []);
+
+  const getSectorData = async () => {
+    try {
+      const sectors = await axios.get(URL.sectors);
+      setSectors(sectors.data.length > 0 ? sectors.data : []);
+      console.log("Finish Loading");
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const onSectorSelection = (event) => {
+    let tempSelectedSectors = [...selectedSectors];
+
+    if (tempSelectedSectors.includes(event.target.value)) {
+      tempSelectedSectors = tempSelectedSectors.filter(
+        (sectorValue) => sectorValue !== event.target.value
+      );
+    } else {
+      tempSelectedSectors.push(event.target.value);
+    }
+
+    setSelectedSectors(tempSelectedSectors);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    validateData();
+  };
+
+  const validateData = () => {
+    if (!name) {
+      toast.error("Please enter a name");
+      return false;
+    } else if (selectedSectors.length === 0) {
+      toast.error("Please select a sector");
+      return false;
+    } else if (!isAgreed) {
+      toast.error("Please agree the terms");
+      return false;
+    } else {
+      toast.success("Success");
+      return true;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Form className="container mt-2">
+      <ToastContainer />
+      <Card body>
+        Please enter your name and pick the Sectors you are currently involved
+        in.
+      </Card>
+      <Form.Group className="mb-3 mt-2" controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter name"
+          onChange={onNameChange}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formSector">
+        <Form.Label>Sectors</Form.Label>
+        <Form.Select
+          aria-label="Select a sector"
+          multiple
+          value={selectedSectors}
+          onChange={onSectorSelection}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {sectors.map((sector, index) => (
+            <option key={index} value={sector.value}>
+              {sector.label}
+            </option>
+          ))}
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check
+          type="checkbox"
+          label="Agree to terms"
+          value={isAgreed}
+          onChange={() => setIsAgreed(!isAgreed)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Save
+      </Button>
+    </Form>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const URL = {
+    sectors: "https://843edfde-5003-4cc2-b26d-94efa693fb7f.mock.pstmn.io/sectors"
+}
